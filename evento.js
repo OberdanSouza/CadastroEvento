b237551b4ab15fd6e685b73ff615de238ac1e790
Oberdan Souza <oberdan.xco@hotmail.com>
2021-12-25 23:07:08 -0300

    CadastroEvento

---
+++ evento.js
@@ -0,0 +1,25 @@
+console.log("Bem vindo!")
+
+let data = 14/01/2022
+
+if (data < 15/01/2022) {
+    console.log("Cadastro permitido!")
+} else {
+    console.log("Cadastro não permitido, data inválida!")
+}
+
+let idade = 18
+
+if (idade >= 18) {
+    console.log("Cadastro permitido!")
+} else {
+    console.log("Cadastro não permitido, menor de idade!")
+}
+
+let quantidade = 101
+
+if (quantidade <= 100) {
+    console.log("Cadastro concluído com sucesso!")
+} else {
+    console.log("Cadastro não permitido, excedido limite de pessoas!")
+}
